621ca3c65a8ce4b70c5b383ac1a71806fbad8590
Shah Kibria <shahkibria@Shahs-MacBook-Air.local>
2021-06-10 01:08:57 -0400

    upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,7 +4,7 @@ library(tidyverse)
 ```
 
 ```{r}
-# Import Data set
+# Import Data set for MechaCar
 mecha_car <- read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors = F )
 ```
 
@@ -12,5 +12,25 @@ mecha_car <- read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors = F
 # Generate multiple regression model amd generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
 ```
+
+```{r}
+# Import Data set for Suspension Coil
+suspension_coil <- read.csv(file="Suspension_Coil.csv",check.names=F,stringsAsFactors = F )
+```
+
+```{r}
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+```
+
+
+
+
+
+
+
+
+
+
 
 
